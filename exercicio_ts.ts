e3affa56b9b0d1f68a4d3886226f003c4c0ff0e6
Gabriel Gontijo <96489350+Gonteejo@users.noreply.github.com>
2023-11-04 17:11:30 -0300

    Update apresentar function to return a string instead of void

--- exercicio_ts.ts
+++ exercicio_ts.ts
@@ -2,8 +2,8 @@ function multiplicar (n1: number, n2: number): number {
     return n1 * n2;
 }
 
-function apresentar (nome: string): void {
-    console.log(`Olá ${nome}`);
+function apresentar (nome: string): string {
+    return `Olá ${nome}`;
 }
 let n1 = parseFloat(prompt('Digite o primeiro número: ') ?? "");
 let n2 = parseFloat(prompt('Digite o segundo número: ') ?? "");
@@ -11,4 +11,4 @@ let n2 = parseFloat(prompt('Digite o segundo número: ') ?? "");
 let resultado: number = multiplicar(n1, n2);
 console.log(resultado);
 let nome: string = prompt ("Digite seu nome: ") ?? "";  
-apresentar(nome);
+console.log(apresentar(nome));
